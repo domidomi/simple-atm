0d384e8b25faefc167e9d89b44d463eef978c3ed
domidomi <dpodgorskad@gmail.com>
2019-04-01 19:47:51 +0200

    Available notes set, state values setup

--- src/components/MoneyWithdraw.js
+++ src/components/MoneyWithdraw.js
@@ -6,7 +6,21 @@ import { Button } from "./UI";
 
 const StyledMoneyWithdraw = styled.div``;
 
-const MoneyWithdraw = props => {
+const AVAILABLE_NOTES = [
+  { id: 0, value: 10, name: "tens" },
+  { id: 1, value: 20, name: "twenties" },
+  { id: 2, value: 50, name: "fifties" },
+  { id: 3, value: 100, name: "hundreds" }
+];
+
+const MoneyWithdraw = () => {
+  const [amount, setAmount] = useState("");
+  const [banknotes, setBanknotes] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const availableNotes = AVAILABLE_NOTES;
   return (
     <StyledMoneyWithdraw>
       <form onSubmit={e => getBanknotes(e)}>
@@ -21,6 +35,17 @@ const MoneyWithdraw = props => {
         <Button type="submit" disabled={isSubmitting}>
           Withdraw
         </Button>
+
+        {isSubmitting && <h2>Submitting</h2>}
+
+        {banknotes && success && (
+          <div>
+            We have following banknotes:
+            {banknotes.map(note => (
+              <div>{note.name}: {note.value}</div>
+            ))}
+          </div>
+        )}
       </form>
     </StyledMoneyWithdraw>
   );
